84694801b7b2347cd62f5a88e5ea29dc1884f98f
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2024-10-10 12:28:54 -0400

    Add http request to the episode controller

--- src/episodes/episodes.controller.ts
+++ src/episodes/episodes.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 
 @Controller('episodes')
-export class EpisodesController {}
+export class EpisodesController {
+  @Get()
+  findAll() {
+    return 'All Episodes';
+  }
+
+  @Get('featured')
+  featuredEpisode(@Query('sort') sort: 'asc' | 'desc' = 'desc') {
+    return 'Feature episodes ' + sort;
+  }
+
+  @Get(':id')
+  findOne(@Param() id: string) {
+    return 'One episode ' + id;
+  }
+
+  @Post()
+  createEpisodes(@Body() input: any) {
+    return `Episode created ${input}`;
+  }
+
+  @Put(':id')
+  updateData(@Param() id: string, @Body() input: any) {
+    return `Data updated with id: ${id} and body ${input}`;
+  }
+
+  @Delete(':id')
+  deleteData(@Param() id: string) {
+    return `Data deleted with id: ${id}`;
+  }
+}
