0b09d28cb8cb3972655640b4a7a35bfa4a49ecd4
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2024-10-10 13:12:00 -0400

    Add a few methods to manage episode data

--- src/episodes/episodes.service.ts
+++ src/episodes/episodes.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { Episode } from './entity/episode.entity';
+import { CreateEpisodeDto } from './dto/create-episodes.dto';
+import { randomUUID } from 'crypto';
 
 @Injectable()
-export class EpisodesService {}
+export class EpisodesService {
+  private episodes: Episode[] = [];
+
+  async findAll(sort: 'asc' | 'desc' = 'asc') {
+    const sortAsc = (a: Episode, b: Episode) => (a.name > b.name ? 1 : -1);
+    const sortDesc = (a: Episode, b: Episode) => (a.name < b.name ? 1 : -1);
+
+    return sort === 'asc'
+      ? this.episodes.sort(sortAsc)
+      : this.episodes.sort(sortDesc);
+  }
+
+  async findFeatured() {
+    return this.episodes.filter((episode) => episode.featured);
+  }
+
+  async findOne(id: string) {
+    return this.episodes.find((episode) => episode.id === id);
+  }
+
+  async createEpisode(createEpisodeDto: CreateEpisodeDto) {
+    const newEpisode = { ...createEpisodeDto, id: randomUUID() };
+    this.episodes.push(newEpisode);
+
+    return newEpisode;
+  }
+}
