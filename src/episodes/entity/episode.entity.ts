4442cb8bc13d0c7546d7966d4caaebfab834c18c
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2024-10-11 21:40:58 -0400

    Create the Episode entity

--- src/episodes/entity/episode.entity.ts
+++ src/episodes/entity/episode.entity.ts
@@ -1,5 +1,25 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
 export class Episode {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column()
   name: string;
+
+  @Column()
   featured?: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
